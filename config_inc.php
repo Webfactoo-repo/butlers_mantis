37b447ce42735b3ab8182dd3e51d72adbd6fa4bf
lordcase <lordcase@gmail.com>
2014-09-29 18:18:25 +0200

    Add - initial upload

---
+++ config_inc.php
@@ -0,0 +1,8 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'butlers_mantis';
+	$g_db_username = 'butlers_mantis';
+	$g_db_password = 'Asdf1234';
+	$g_reauthentication_expiry = 100*60;
+?>
